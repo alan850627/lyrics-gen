1bb1027cf81ac7d95f5a683ce3ae1d3103638420
alan850627 <alan850627@gmail.com>
2018-05-02 02:11:10 -0500

    scrape script written

---
+++ scrape.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python gather.py --output_file country.txt --artists "137438973836, 2011, 11114"
+python gather.py --output_file jazz.txt --artists "137438960693, 9541, 137438969163"
+python gather.py --output_file latin.txt --artists "857, 137438962670, 4579"
+python gather.py --output_file rap.txt --artists "291, 94, 6711, 6627"
+python gather.py --output_file children.txt --artists "137438962697, 311, 137439010697"
